a4b6329705223c40ff1afcbd535dd86671f49606
김영호 <bigtiger92@naver.com>
2020-07-22 17:21:29 +0900

    0722 youngho

--- backend/src/main/java/com/web/blog/model/comment/Comment.java
+++ backend/src/main/java/com/web/blog/model/comment/Comment.java
@@ -1,17 +1,21 @@
-package com.web.blog.model.post;
+package com.web.blog.model.comment;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+
+import org.hibernate.annotations.ManyToAny;
 
 
 @Entity
@@ -19,17 +23,16 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@Table(name = "article") // 이어줄 테이블명 지정
+@Table(name = "comment") // 이어줄 테이블명 지정
 
-public class Post {
+public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int pid;
+    private int no;
     
-    private String title;
-    private int memberAmount;
-    private int price;
+    private String content;
     private String writer;
-    private String description;
+
+    private int articleno;
     
 }
