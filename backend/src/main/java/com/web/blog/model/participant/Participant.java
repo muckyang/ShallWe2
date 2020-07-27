814d398e24ddf72c45445275c44f0f827c761040
송복민 <bockmin@gmail.com>
2020-07-27 15:00:55 +0900

    0727_participant,category 모델 커밋합니다.

---
+++ backend/src/main/java/com/web/blog/model/participant/Participant.java
@@ -0,0 +1,27 @@
+package com.web.blog.model.participant;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Table(name = "participant") // 이어줄 테이블명 지정
+
+public class Participant {
+
+    private int userId;
+    private int articleId;
+    private String title;
+    private int price;
+    private String description;
+
+}
